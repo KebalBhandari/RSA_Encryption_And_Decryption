ab6593d70173087f3a4cca9b6eaf16b640ee1195
Kebal Bhandari <37100828+KebalBhandari@users.noreply.github.com>
2024-04-08 14:54:46 +0300

    Update rsa_encryption_decryption.py

--- rsa_encryption_decryption.py
+++ rsa_encryption_decryption.py
@@ -1,1 +1,15 @@
-
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import binascii
+import time
+start_time = time.time()
+key = RSA.generate(1024) 
+public_key = key.publickey()
+message = 'Hello World !'
+encryptor = PKCS1_OAEP.new(public_key)
+encrypted_message = encryptor.encrypt(message.encode())
+print("Encrypted:", binascii.hexlify(encrypted_message))
+decryptor = PKCS1_OAEP.new(key)
+decrypted_message = decryptor.decrypt(encrypted_message)
+print("Decrypted:", decrypted_message.decode())
+print("--- %s seconds ---" % (time.time() - start_time))
